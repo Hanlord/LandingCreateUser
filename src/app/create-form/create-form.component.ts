b92a918a829699c50b657262b2864da48bf65672
We-Han <chen007@live.at>
2023-04-16 15:50:33 +0200

    update validation

--- src/app/create-form/create-form.component.ts
+++ src/app/create-form/create-form.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
-import { FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
@@ -26,11 +24,21 @@ export class AppModule { }
   styleUrls: ['./create-form.component.scss']
 })
 export class CreateFormComponent implements OnInit {
-  
+  formInfo: FormGroup | undefined;
   constructor() { }
-
+   
   ngOnInit(): void {
-    
+    ({
+      fname: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(25)]],
+      lname: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: ['', [Validators.required, Validators.pattern(/^\+[0-9]{1,3}[0-9]{3,14}$/)]],
+      fcheck: ['', [Validators.requiredTrue]]
+    });
   }
-
+  onSubmit(): void {
+    if (this.formInfo && this.formInfo.valid) {
+      const formData = this.formInfo.value;
+      this.formInfo.reset();
+  }}
 }
